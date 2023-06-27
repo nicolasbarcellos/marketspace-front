00d8a8fde5e2a291541a9c055ae4368a33574bc3
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2023-06-27 00:25:51 -0300

    add font

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
+import { Text, StatusBar } from 'react-native'
+import customThemeType from './src/styles/theme'
+import {
+  useFonts,
+  Karla_400Regular,
+  Karla_700Bold,
+} from '@expo-google-fonts/karla'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider theme={customThemeType}>
+      {fontsLoaded && (
+        <Text>Open up App.tsx to start worsking on your app!</Text>
+      )}
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle={'light-content'}
+      />
+    </NativeBaseProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,41 @@
+import { extendTheme } from 'native-base'
+
+const customThemeType = extendTheme({
+  colors: {
+    blue: {
+      700: '#364d9d',
+      500: '#647ac7',
+    },
+    gray: {
+      700: '#1A181B',
+      600: '#3E3A40',
+      500: '#5F5B62',
+      400: '#9F9BA1',
+      300: '#D9D8DA',
+      200: '#EDECEE',
+      100: '#F7F7F8',
+    },
+    white: '#FFFFFF',
+    red: {
+      500: '#EE7979',
+    },
+  },
+
+  fonts: {
+    heading: 'Karla_700Bold',
+    body: 'Karla_400Regular',
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 20,
+    xl: 24,
+  },
+  sizes: {
+    14: 56,
+    33: 148,
+  },
+})
+
+export default customThemeType
